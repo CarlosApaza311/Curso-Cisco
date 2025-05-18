4dbda7ab34111f13d5af9b8709f5fea5e5d0778b
CarlosApaza311 <carlos.j.apaza.g@gmail.com>
2025-05-18 00:40:33 -0500

    modlu 3.1

--- Curso Cisco/s3.2.7.py
+++ Curso Cisco/s3.2.7.py
@@ -6,6 +6,5 @@ for i in range(1,6):
     print(i,"Mississippi")
     time.sleep(1)
 # Body of the loop - use: time.sleep(1)
-time.sleep(1)
 # Write a print function with the final message.
 print("Ready or not, here I come!")
